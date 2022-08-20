8b5495bd758ce5ef53674dbd9e25e7e8ec0f03da
area31Joel <area31@163.com>
2022-08-20 12:32:40 +0800

    master

--- src/QRcodeUtil.java
+++ src/QRcodeUtil.java
@@ -1,9 +1,11 @@
 import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.UUID;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -21,8 +23,12 @@ public class QRcodeUtil {
     public void getQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
         //生成二维码类
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        //设置字符编号
+        HashMap<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
         //生成的二维码
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height,hints);
+        //BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
         //生成图片唯一名称,加上.png格式
         String pat = getUUID() + ".png";
         //图片路劲加上图片名称  (输出地址)
